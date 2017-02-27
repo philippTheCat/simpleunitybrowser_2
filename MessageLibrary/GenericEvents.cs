495a9ab3e6a431f4320618d3cc0a1f068fe57a3b
Vitaly Chashin <wchfgpl@gmail.com>
2017-02-27 16:02:06 +0000

    README.md edited online with Bitbucket

---
+++ MessageLibrary/GenericEvents.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+
+namespace MessageLibrary
+{
+
+    public enum BrowserEventType
+    {
+        Ping=-1,
+        Generic=0,
+        Mouse=1,
+        Keyboard=2,
+        Dialog = 3
+    }
+
+    [Serializable]
+    public class AbstractEvent
+    {
+        public BrowserEventType GenericType;//?
+    }
+
+    [Serializable]
+    public class EventPacket
+    {
+        public BrowserEventType Type;
+
+        public AbstractEvent Event;
+    }
+
+    public enum GenericEventType
+    {
+        Shutdown=0,
+        Navigate=1,
+        GoBack=2,
+        GoForward=3,
+        ExecuteJS=4,
+        JSQuery=5,
+        JSQueryResponse=6,
+        PageLoaded=7
+
+        
+    }
+
+   
+
+    [Serializable]
+    public class GenericEvent : AbstractEvent
+    {
+        public GenericEventType Type;
+
+        public string NavigateUrl;
+
+        public string JsCode;
+
+        public string JsQuery;
+
+        public string JsQueryResponse;
+    }
+}
---
+++ MessageLibrary/KeyboardEvents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace MessageLibrary
+{
+  public enum KeyboardEventType
+    {
+        CharKey=0,
+        Down=1,
+        Up=2,
+        Focus=3
+    
+    }
+
+    [Serializable]
+    public class KeyboardEvent : AbstractEvent
+    {
+        public KeyboardEventType Type;
+        public int Key;
+    }
+}
